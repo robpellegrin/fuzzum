fe38e189b63769ef8d3e30df974f9bb0e6b96737
Rob Pellegrin <>
2026-03-13 21:20:23 -0400

    search_pane.py: Introduced needs_refresh attribute. Removed redundant screen refreshes. Added method to get cursor position to keep cursor within search box.

--- ui/panes/search_pane.py
+++ ui/panes/search_pane.py
@@ -8,23 +8,32 @@
 """
 
 import curses
+import sys
 
 from ui.base_window import BaseWindow
 
 
 class SearchPane(BaseWindow):
+
+    def __init__(self, app, name):
+        super().__init__(app, name)
+
     def create(self):
         self.win = curses.newwin(3, self.width // 2, self.height - 3, 0)
-
-    def refresh(self) -> None:
-        self.win.erase()
         self.win.box()
+
+    def draw(self) -> None:
         self.win.addstr(0, 2, " Search ", curses.color_pair(3))
+        self.win.addstr(1, 2, "> " + self.app.query, curses.color_pair(1))
+        self.stdscr.move(self.height - 2, 4 + len(self.app.query))
 
     def update_query(self, query: str) -> None:
-        self.win.addstr(1, 2, "> " + query, curses.color_pair(1))
-        self.stdscr.move(self.height - 2, 4 + len(query))
-        self.win.noutrefresh()
+        self.app.query += query
+        self.needs_refresh = True
 
     def resize(self, height, width):
         self.win.resize(height, width)
+        self.needs_refresh = True
+
+    def get_cursor_position(self):
+        return (self.height - 2, 4 + len(self.app.query))
