5be26f6aa945309eedc22e514b9aa4877b0b6b7c
Rob Pellegrin <>
2026-03-14 21:33:19 -0400

    details_pane.py: Added: logging, typehints.

--- ui/panes/details_pane.py
+++ ui/panes/details_pane.py
@@ -3,43 +3,52 @@
 @author  Rob Pellegrin
 @date    03-11-2026
 
-@updated 03-11-2026
+@updated 03-14-2026
 
 """
 
 import curses
+import logging
 import os
 import time
 
 from ui.base_window import BaseWindow
 
+logger = logging.getLogger(__name__)
+
 
 class DetailsPane(BaseWindow):
 
-    def create(self):
+    def create(self) -> None:
         self.win = curses.newwin(
             3, self.width // 2, self.height - 3, self.width // 2
         )
 
-    def draw(self):
+    def draw(self) -> None:
         self.win.box()
         self.win.addstr(0, 2, " Details ", curses.color_pair(3))
-        self._stat_file()
 
-    def _stat_file(self):
-        info = os.stat(self.app.files[self.app.cursor])
+        details_str: str = self._stat_file()
+
+        self.win.addstr(1, 2, details_str)
+
+    def _stat_file(self) -> str:
+        selected_file: str = self.app.files[self.app.cursor]
+        info: os.stat_result = os.stat(selected_file)
 
         # File size in bytes
-        size = info.st_size
+        size: int = info.st_size
 
         # Last modification time (timestamp)
-        mtime = time.strftime(
+        mtime: str = time.strftime(
             "%Y-%m-%d %H:%M:%S", time.localtime(info.st_mtime)
         )
 
         # Permissions as octal
-        perms = oct(info.st_mode & 0o777)
+        perms: str = oct(info.st_mode & 0o777)
 
-        self.win.addstr(1, 2, f"{size} {mtime} {perms}")
+        logger.debug("DetailsPane has file: %s", selected_file)
 
         self.needs_refresh = True
+
+        return f"{size} {mtime} {perms}"
