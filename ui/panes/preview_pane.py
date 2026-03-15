496cbc9facf86f7cad422580416c66348bacbe00
Rob Pellegrin <>
2026-03-15 00:07:54 -0400

    preview_pane.py: Added type hints.

--- ui/panes/preview_pane.py
+++ ui/panes/preview_pane.py
@@ -3,7 +3,7 @@
 @author  Rob Pellegrin
 @date    03-11-2026
 
-@updated 03-11-2026
+@updated 03-14-2026
 
 """
 
@@ -13,12 +13,12 @@ from ui.base_window import BaseWindow
 
 
 class PreviewPane(BaseWindow):
-    def create(self):
+    def create(self) -> None:
         left_width = self.width // 2
         right_width = self.width - left_width
 
         self.win = curses.newwin(self.height - 3, right_width, 0, left_width)
 
-    def draw(self):
+    def draw(self) -> None:
         self.win.box()
         self.win.addstr(0, 2, " Preview ", curses.color_pair(3))
