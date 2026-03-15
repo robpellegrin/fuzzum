00bf06319d84757cb07600ea202473475176802a
Rob Pellegrin <>
2026-03-14 23:48:25 -0400

    config.py: Added type hints. Expanded commenting.

--- utils/config.py
+++ utils/config.py
@@ -3,51 +3,72 @@
 @author  Rob Pellegrin
 @date    03-11-2026
 
-@updated 03-11-2026
+@updated 03-14-2026
 
 """
 
 import json
 from pathlib import Path
-
-CONFIG_DIR = Path.home() / ".config" / "myapp"
-CONFIG_FILE = CONFIG_DIR / "config.json"
+from typing import Any
 
 
 class Config:
-    def __init__(self):
+
+    CONFIG_DIR = Path.home() / ".config" / "myapp"
+    CONFIG_FILE = CONFIG_DIR / "config.json"
+
+    def __init__(self) -> None:
         self.data = {
-          "panes": {
-            "preview": False,
-            "details": True
-          },
-          "ui": {
-            "show_hidden_files": True,
-            "show_filenames_only": False
-          }
+            "panes": {"preview": False, "details": True},
+            "ui": {"show_hidden_files": True, "show_filenames_only": False},
         }
 
         self.load()
 
-    def load(self):
-        if CONFIG_FILE.exists():
-            with open(CONFIG_FILE) as f:
+    def load(self) -> None:
+        """
+        Loads the contents of the config file.
+
+        """
+
+        if self.CONFIG_FILE.exists():
+            with open(self.CONFIG_FILE, encoding="UTF-8") as f:
                 self.data.update(json.load(f))
 
-    def save(self):
-        CONFIG_DIR.mkdir(parents=True, exist_ok=True)
-        with open(CONFIG_FILE, "w") as f:
+    def save(self) -> None:
+        """
+        Writes the state of self.data to config file.
+
+        """
+
+        self.CONFIG_DIR.mkdir(parents=True, exist_ok=True)
+        with open(self.CONFIG_FILE, "w", encoding="UTF-8") as f:
             json.dump(self.data, f, indent=4)
 
-    def get(self, *keys):
-        value = self.data
+    def get(self, *keys: str) -> Any:
+        """
+        Returns the value corresponding to the supplied key.
+
+        """
+
+        value: Any = self.data
+
         for k in keys:
             value = value[k]
+
         return value
 
-    def set(self, value, *keys):
-        d = self.data
+    def set(self, value: Any, *keys: str) -> None:
+        """
+        Updates a config value, then writes the new state to config
+        file.
+
+        """
+
+        d: dict[str, Any] = self.data
+
         for k in keys[:-1]:
             d = d.setdefault(k, {})
+
         d[keys[-1]] = value
         self.save()
