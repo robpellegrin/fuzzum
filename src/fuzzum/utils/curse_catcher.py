2d7bdb31f3afa58264984c28d0272dbdff5258f8
Rob Pellegrin <>
2026-07-06 22:32:00 -0400

    Expanded type hints.

--- src/fuzzum/utils/curse_catcher.py
+++ src/fuzzum/utils/curse_catcher.py
@@ -1,10 +1,13 @@
 import curses
 import functools
+from typing import Any, Callable, Optional, TypeVar
+
+R = TypeVar("R")
 
 
-def curse_catch(func):
+def curse_catch(func: Callable[..., R]) -> Callable[..., Optional[R]]:
     @functools.wraps(func)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args: Any, **kwargs: Any) -> Optional[R]:
         try:
             return func(*args, **kwargs)
         except curses.error:
